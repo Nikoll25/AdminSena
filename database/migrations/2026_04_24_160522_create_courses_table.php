c83661a1edb62e1ed506551163a771fa8dd35e63
nikoll Guaca <guacanikoll@gmail.com>
2026-04-24 11:51:20 -0500

    Migraciones

---
+++ database/migrations/2026_04_24_160522_create_courses_table.php
@@ -0,0 +1,47 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('courses', function (Blueprint $table) {
+            $table->id();
+            $table->string('number');
+            $table->tinyInteger('day');
+
+            //creo la llave foranea para area
+            $table->unsignedBigInteger('area_id')->nullable()->unique();
+
+            $table->foreign('area_id')
+                ->references('id')
+                ->on('areas')
+                ->onDelete('set null')
+                ->onUpdate('set null');
+
+            //creo la llave foranea para training_Center
+            $table->unsignedBigInteger('training_center_id')->nullable()->unique();
+
+            $table->foreign('training_center_id')
+                ->references('id')
+                ->on('training_centers')
+                ->onDelete('set null')
+                ->onUpdate('set null');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('courses');
+    }
+};
